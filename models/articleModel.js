ec45f55011f9e385a19294653b3e84fc4c2aa28e
PratikshaSh <pratiksha8520@gmail.com>
2021-05-18 01:26:49 +0530

    create schema for articles

---
+++ models/articleModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+
+const articleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+
+},{
+    timestamps: true
+});
+
+const Article = mongoose.model('article', articleSchema);
+
+module.exports = Article;
